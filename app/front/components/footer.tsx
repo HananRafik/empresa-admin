f3416ff1fcdb8c4f587c18e378da8479ab50ae43
Hanan-rafik <208627609+HananRafik@users.noreply.github.com>
2025-04-22 14:54:26 +0200

    update

---
+++ app/front/components/footer.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function footer() {
+  return (
+    <footer>
+      &copy; {new Date().getFullYear()} Todos los derechos reservados
+    </footer>
+  )
+}
