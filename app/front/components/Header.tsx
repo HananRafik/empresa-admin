cf414cf8f5ebd7df4496abe49722452ea18dc9f9
Hanan-rafik <208627609+HananRafik@users.noreply.github.com>
2025-04-22 16:57:28 +0200

    update

--- app/front/components/Header.tsx
+++ app/front/components/Header.tsx
@@ -1,32 +1,11 @@
 'use client'
 import React from 'react'
-import {
-    Button, 
-    Menu, 
-    MenuProps, 
-    Switch, 
-    Typography, 
-    Avatar, 
-    Dropdown, 
-    Space
-} from 'antd';
-
-import {
-    MoonOutlined, 
-    SunOutlined, 
-    MenuOutlined, 
-    UserOutlined
-} from '@ant-design/icons'
-
-import Link from 'next/link';
-
-    
 
 
 
 export default function Header() {
   return (
-   <header>
+   <header className='bg-white'>
 
    </header>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from '../app/front/components/Header'
 import Footer from '../app/front/components/footer'
 
-import 'ant/dist/reset.css'
+
 export default function Home() {
   return (
 
