02ab474fc5c7b389a2da3f1332efbf1a4c82f021
HananRafik <208627609+HananRafik@users.noreply.github.com>
2025-04-23 14:05:53 +0200

    update

--- app/precios/page.tsx
+++ app/precios/page.tsx
@@ -1,8 +1,24 @@
+'use client'
+
+
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/footer'
+
 
 export default function page() {
   return (
-    <div>
+    <div className='bg-gray-50 min-h-screen'>
+        <Header/>
+
+
+        <main className='container'>
+
+        </main>
+
+
+
+        <Footer/>
       
     </div>
   )
